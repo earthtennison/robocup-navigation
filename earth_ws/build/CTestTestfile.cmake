f82a815625db9c0dfd3c7e2cbb523175764ffca8
Pirat <pirat.pot@gmail.com>
2021-10-29 14:20:54 +0700

    Delete Eart ws

--- earth_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/earth10/robocup-navigation/earth_ws/src
-# Build directory: /home/earth10/robocup-navigation/earth_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("beginner_tutorials")
